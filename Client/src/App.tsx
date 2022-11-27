0bd34478cdc54c58d5ad27faeae62f2d521e6f37
Carl Åfalk <carl.afalk@hotmail.se>
2022-11-27 18:04:38 +0100

    added routing

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,9 +1,26 @@
+import { Route, Routes } from "react-router-dom"
+import GlobalStyle from "./globalStyles"
+import Layout from "./Layout"
+import { Game } from "./pages/Game"
+import { Highscore } from "./pages/Highscore"
+import { Login } from "./pages/Login"
+import { Profile } from "./pages/Profile"
 
 function App() {
 
   return (
-   <>
-   </>
+  <>
+   <GlobalStyle />
+   <Routes>
+     <Route path="/" element={<Layout/>}>
+        <Route index element={<Login />}/>
+        <Route path="game/" element={<Game />}/>
+        <Route path="profile/" element={<Profile/>}>
+          <Route path="highscore/" element={<Highscore/>}/>
+        </Route>
+     </Route>
+   </Routes>
+  </>
   )
 }
 
