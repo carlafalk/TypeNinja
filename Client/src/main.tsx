297fdb964a77680a49d816872029ff1a7632841d
Carl Åfalk <carl.afalk@hotmail.se>
2022-11-27 18:06:39 +0100

    globalstyle + dependencies!

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import CurrentUserProvider from "./contexts/CurrentUserContext";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <CurrentUserProvider>
+        <App />
+      </CurrentUserProvider>
+    </BrowserRouter>
   </React.StrictMode>
 )
