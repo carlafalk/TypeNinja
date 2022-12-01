c88c2a4306083a7cda8c69ad41416701b63aedf3
Carl Åfalk <carl.afalk@hotmail.se>
2022-12-01 16:01:21 +0100

    localstorage hook added

--- Client/src/pages/Game.tsx
+++ Client/src/pages/Game.tsx
@@ -1,6 +1,13 @@
+import { useCurrentUser } from "../contexts/CurrentUserContext";
 
 export const Game = () => {
+    // const [currentUser, setCurrentUser] = useLocalStorage<CurrentUser>("currentUser", {username:"", token:"", isLoggedIn:false});
+    const { currentUser ,setCurrentUser } = useCurrentUser();
+
+
   return (
-    <div>Game</div>
+    <>
+    <div style={{marginTop: 100}}>{currentUser.username}</div>
+    </>
   )
 }
