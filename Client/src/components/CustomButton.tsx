6fc74eb1aaf4bfa583a377690f5810a4f92052a6
Carl Åfalk <carl.afalk@hotmail.se>
2022-12-05 18:23:53 +0100

    created button component

---
+++ Client/src/components/CustomButton.tsx
@@ -0,0 +1,26 @@
+import { Button } from '@mui/material';
+import { styled as styledMUI } from '@mui/material/styles';
+import { ReactNode } from 'react';
+
+interface CustomButtonProps{
+    children: ReactNode;
+    onClick: () => void;
+}
+
+export const CustomButton = ({onClick, children} : CustomButtonProps) => {
+  return (
+    <MUIButton 
+    variant="contained"
+    onClick={onClick}>{children}</MUIButton>
+  )
+}
+const MUIButton = styledMUI(Button)`
+  width: 200px; 
+  margin: 10px;
+  color: #bfa5d7;
+  background-color: #1f0438;
+  border-radius: 5rem;
+  &:hover {
+    background-color: #42136e;
+  }
+`;
