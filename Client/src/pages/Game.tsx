13150be2833cf4041b56576f16b88714c0467650
Carl Åfalk <carl.afalk@hotmail.se>
2022-12-12 18:28:06 +0100

    mock words and map them in game/

--- Client/src/pages/Game.tsx
+++ Client/src/pages/Game.tsx
@@ -1,19 +1,50 @@
-import { useQuery } from "react-query";
+import { Box, Typography } from "@mui/material";
+import { styled as styledMUI } from "@mui/material/styles";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
 import MainContent from "../components/MainContent";
-import { useCurrentUser } from "../contexts/CurrentUserContext";
-import useAxios from "../hooks/useAxios";
+import { arrayOfWords } from "../utils/WordArray";
 
 export const Game = () => {
-    // const [currentUser, setCurrentUser] = useLocalStorage<CurrentUser>("currentUser", {username:"", token:"", isLoggedIn:false});
-    const { currentUser ,setCurrentUser } = useCurrentUser();
-    const fetchWords = useAxios({url: "https://api.datamuse.com/words?sl=jirraf", method: "get"});
-    const { data } = useQuery<string[]>(["words"], fetchWords);
+  const [keyPressed, setKeyPressed] = useState("");
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyPress, true);
+  }, []);
+
+  //if double letter (i.e "ll") then it will only log "l" once...
+  useEffect(() => {
+    console.log(keyPressed);
+  }, [keyPressed]);
+
+  const handleKeyPress = (event: KeyboardEvent) => {
+    setKeyPressed(event.key);
+    // handle double letters here with if statement
+  };
 
   return (
     <>
-    <MainContent>
-      {currentUser.username}
-    </MainContent>
+      <MainContent>
+        <WordContainer>
+          {arrayOfWords.map((t) => (
+            <Typography sx={{ color: "#0009", fontFamily: "Saira Condensed", fontSize: 40, marginRight: 2, letterSpacing: 4 }} key={t}>
+              {t}
+            </Typography>
+          ))}
+        </WordContainer>
+      </MainContent>
     </>
-  )
-}
+  );
+};
+
+const WordContainer = styledMUI(Box)`
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 300px;
+`;
+
+const Test = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 300px;
+`;
