ab346f52b4d9783cac2ab545dc00aeb090e8b932
Carl Åfalk <carl.afalk@hotmail.se>
2022-12-22 17:52:54 +0100

    using wordarray instead of single long string

--- Client/src/pages/Game.tsx
+++ Client/src/pages/Game.tsx
@@ -6,18 +6,27 @@ import words from "../assets/Words.json";
 import MainContent from "../components/MainContent";
 
 export const Game = () => {
-  const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const [currentWordIndex, setCurrentWordIndex] = useState<number>(0);
+  const [currentLetterIndex, setCurrentLetterIndex] = useState<number>(0);
   const [shuffledWordList, setShuffledWordList] = useState<string[]>(words.sort(() => 0.5 - Math.random()).slice(0, 100));
 
   const handleRestart = () => {
-    setCurrentIndex(0);
+    setCurrentWordIndex(0);
+    setCurrentLetterIndex(0);
     // reset timer
     // reset all colors
   };
 
   const handleBackspace = () => {
-    currentIndex > 0 ? setCurrentIndex((prev) => prev - 1) : setCurrentIndex(0);
+    currentLetterIndex > 0 ? setCurrentLetterIndex((prev) => prev - 1) : setCurrentLetterIndex(0);
     // reset color on last character
+  };
+  const handleSpace = () => {
+    if (shuffledWordList[currentWordIndex].length === currentLetterIndex) {
+      setCurrentWordIndex((prev) => prev + 1);
+      setCurrentLetterIndex(0);
+      //add point for correct word
+    } else console.log("asdasd");
   };
 
   const handleKeyPress = (key: string): string => {
@@ -28,9 +37,12 @@ export const Game = () => {
       case "Backspace":
         handleBackspace();
         break;
+      case " ":
+        handleSpace();
+        break;
 
       default:
-        setCurrentIndex((prev) => prev + 1);
+        setCurrentLetterIndex((prev) => prev + 1);
         break;
     }
     return key;
@@ -42,13 +54,29 @@ export const Game = () => {
         <WordsContainer
           tabIndex={0}
           onKeyDown={(e: React.KeyboardEvent<HTMLDivElement>) => {
-            handleKeyPress(e.key) === shuffledWordList[currentIndex] ? console.log("correct") : console.log("incorrect");
+            if (handleKeyPress(e.key) === shuffledWordList[currentWordIndex][currentLetterIndex]) {
+              console.log("correct");
+            } else if (e.key === " ") {
+              console.log("space pressed");
+            } else if (e.key === "Backspace") {
+              console.log("backspace pressed");
+            } else if (e.key === "Tab") {
+              console.clear();
+            } else {
+              console.log("incorrect");
+            }
+            // handleKeyPress(e.key) === shuffledWordList[currentWordIndex][currentLetterIndex] ? console.log("correct") : console.log("incorrect");
+            // console.log(e.key);
+            // console.log(currentLetterIndex);
+
             e.preventDefault();
           }}
         >
           {shuffledWordList.map((element) => (
             <WordContainer>
-              <Word sx={{ color: "#0009" }}>{element}</Word>
+              <>
+                <Word sx={{ color: "#0009" }}>{element}</Word>
+              </>
             </WordContainer>
           ))}
         </WordsContainer>
