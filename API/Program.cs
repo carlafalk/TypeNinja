d18bfaf13dddc2e0363666ecae008c04df03b700
Carl Åfalk <carl.afalk@hotmail.se>
2023-05-08 17:11:41 +0200

    moved dependencies to program

--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,62 @@
+using System.Text;
+using AppCore.Infrastructure;
 using Infrastructure;
-
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-Dependencies.ConfigServices(builder.Configuration, builder.Services);
+// Dependencies.ConfigServices(builder.Configuration, builder.Services);
+
+//adding sqlite db 
+
+builder.Services.AddDbContext<Context>(c => c.UseSqlite(builder.Configuration.GetConnectionString("Db")));
+
+var test = Directory.GetCurrentDirectory();
+var directory = new DirectoryInfo(Directory.GetCurrentDirectory());
+var dotenvPath = Path.Combine(directory.Parent.FullName, ".env");
+DotEnv.Load("C:\\skola\\TypeNinja\\.env");
+// DotEnv.Load(dotenvPath);
+
+
+var configBuilder = new ConfigurationBuilder().AddEnvironmentVariables().Build();
+
+builder.Services.AddIdentityCore<IdentityUser>(opt => 
+{
+    //password settings 
+
+    opt.Password.RequireDigit = true;
+    opt.Password.RequiredLength = 6;
+    opt.Password.RequireNonAlphanumeric = false;
+    opt.Password.RequireUppercase = false;
+    opt.Password.RequireLowercase = false;
+    opt.Password.RequiredUniqueChars = 1;
+})
+.AddEntityFrameworkStores<Context>()
+.AddDefaultTokenProviders();
+builder.Services.AddCors(options => options.AddDefaultPolicy(builder => builder.WithOrigins("http://127.0.0.1:5173").AllowAnyHeader().AllowAnyMethod().AllowCredentials()));
+
+builder.Services.AddAuthentication(options => {
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options => 
+{
+    options.SaveToken = true;
+    options.RequireHttpsMetadata = false;
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration.GetValue<string>("JWT:ValidAudience"),
+        ValidIssuer = builder.Configuration.GetValue<string>("JWT:ValidIssuer"),
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetValue<string>("JWT:Secret")))
+    };
+});
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
--- AppCore/Infrastructure/Dependencies.cs
+++ AppCore/Infrastructure/Dependencies.cs
@@ -1,64 +1,64 @@
-using AppCore.Entities;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-using AppCore.Infrastructure;
-using Microsoft.Extensions.Options;
+// using AppCore.Entities;
+// using Microsoft.EntityFrameworkCore;
+// using Microsoft.Extensions.Configuration;
+// using Microsoft.Extensions.DependencyInjection;
+// using Microsoft.AspNetCore.Authentication.JwtBearer;
+// using Microsoft.AspNetCore.Identity;
+// using Microsoft.IdentityModel.Tokens;
+// using System.Text;
+// using AppCore.Infrastructure;
+// using Microsoft.Extensions.Options;
 
-namespace Infrastructure;
-public static class Dependencies
-{
-    public static void ConfigServices(IConfiguration config, IServiceCollection services)
-    {
-        //adding sqlite db 
-        services.AddDbContext<Context>(c => c.UseSqlite(config.GetConnectionString("Db")));
+// namespace Infrastructure;
+// public static class Dependencies
+// {
+//     public static void ConfigServices(IConfiguration config, IServiceCollection services)
+//     {
+//         //adding sqlite db 
+//         services.AddDbContext<Context>(c => c.UseSqlite(config.GetConnectionString("Db")));
 
-        var test = Directory.GetCurrentDirectory();
-        var directory = new DirectoryInfo(Directory.GetCurrentDirectory());
-        var dotenvPath = Path.Combine(directory.Parent.FullName, ".env");
-        DotEnv.Load("C:\\skola\\TypeNinja\\.env");
-        // DotEnv.Load(dotenvPath);
+//         var test = Directory.GetCurrentDirectory();
+//         var directory = new DirectoryInfo(Directory.GetCurrentDirectory());
+//         var dotenvPath = Path.Combine(directory.Parent.FullName, ".env");
+//         DotEnv.Load("C:\\skola\\TypeNinja\\.env");
+//         // DotEnv.Load(dotenvPath);
         
 
-        var configBuilder = new ConfigurationBuilder().AddEnvironmentVariables().Build();
+//         var configBuilder = new ConfigurationBuilder().AddEnvironmentVariables().Build();
 
-        services.AddIdentityCore<IdentityUser>(opt => 
-        {
-            //password settings 
+//         services.AddIdentityCore<IdentityUser>(opt => 
+//         {
+//             //password settings 
 
-            opt.Password.RequireDigit = true;
-            opt.Password.RequiredLength = 6;
-            opt.Password.RequireNonAlphanumeric = false;
-            opt.Password.RequireUppercase = false;
-            opt.Password.RequireLowercase = false;
-            opt.Password.RequiredUniqueChars = 1;
-        })
-        .AddEntityFrameworkStores<Context>()
-        .AddDefaultTokenProviders();
-        services.AddCors(options => options.AddDefaultPolicy(builder => builder.WithOrigins("http://127.0.0.1:5173").AllowAnyHeader().AllowAnyMethod().AllowCredentials()));
+//             opt.Password.RequireDigit = true;
+//             opt.Password.RequiredLength = 6;
+//             opt.Password.RequireNonAlphanumeric = false;
+//             opt.Password.RequireUppercase = false;
+//             opt.Password.RequireLowercase = false;
+//             opt.Password.RequiredUniqueChars = 1;
+//         })
+//         .AddEntityFrameworkStores<Context>()
+//         .AddDefaultTokenProviders();
+//         services.AddCors(options => options.AddDefaultPolicy(builder => builder.WithOrigins("http://127.0.0.1:5173").AllowAnyHeader().AllowAnyMethod().AllowCredentials()));
 
-        services.AddAuthentication(options => {
-            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-        })
-        .AddJwtBearer(options => 
-        {
-            options.SaveToken = true;
-            options.RequireHttpsMetadata = false;
-            options.TokenValidationParameters = new TokenValidationParameters()
-            {
-                ValidateIssuer = true,
-                ValidateAudience = true,
-                ValidAudience = config["JWT:ValidAudience"],
-                ValidIssuer = config["JWT:ValidIssuer"],
-                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["JWT:Secret"]))
-            };
-        });
+//         services.AddAuthentication(options => {
+//             options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+//             options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+//             options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+//         })
+//         .AddJwtBearer(options => 
+//         {
+//             options.SaveToken = true;
+//             options.RequireHttpsMetadata = false;
+//             options.TokenValidationParameters = new TokenValidationParameters()
+//             {
+//                 ValidateIssuer = true,
+//                 ValidateAudience = true,
+//                 ValidAudience = config["JWT:ValidAudience"],
+//                 ValidIssuer = config["JWT:ValidIssuer"],
+//                 IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["JWT:Secret"]))
+//             };
+//         });
 
-    }
-}
+//     }
+// }
